700a05a224281ec81aae3c20b63f3ecae6be8ea5
Leduong1 <99656475+Leduong1@users.noreply.github.com>
2022-03-08 17:56:53 +0700

    Add files via upload
    create

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = '';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100039192007042';
+const eventName = 'GỬI TẶNG THÙY LINH😘😘🥰🥰🥰';
+const titleCard = 'Tặng Thùy Linh';
+const contentCard = 'Chúc bé 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bé trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'linh.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
